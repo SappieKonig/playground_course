fd72ccd9846493e14b686d5b641cf483ee41bd3d
SappieKonig <sappie.konig@gmail.com>
2021-02-11 16:48:16 +0100

    first commit
    Signed-off-by: SappieKonig <sappie.konig@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import gym
 from models import get_model
+import numpy as np
 from funcs import decay_and_normalize
+import random
 
 
 # create gym
@@ -8,32 +10,59 @@ env = gym.make("CartPole-v0")
 env.reset()
 
 # assign model to variable
-model = get_model()
+model = get_model(3e-4)
 
 # totaal
-all_obs = []
-all_rewards = []
-all_done = []
-for i in range(100):
-    # game niveau
-    observations = []
-    rewards = []
-    dones = []
-    done = False
-    while not done:
-        # turn niveau, we hebben info niet nodig
-        observation, reward, done, _ = env.step(0)
-        observations.append(observation)
-        rewards.append(reward)
-        dones.append(done)
-        env.render()
 
-    # laatste turn voor game over
-    env.reset()
-    all_done.append(dones)
-    all_rewards.append(rewards)
-    all_obs.append(observations)
+for _ in range(1000):
+    all_obs = []
+    all_rewards = []
+    all_done = []
+    all_actions = []
+    for i in range(3):
+        # game niveau
+        observations = []
+        rewards = []
+        dones = []
+        done = False
+        actions = []
+        obs = env.reset()
+        while not done:
+            # turn niveau, we hebben info niet nodig
+            p = model(obs.reshape(1, -1))[0]
+
+            if random.random() < p:
+                action = 1
+            else: action = 0
+
+            observations.append(obs)
+            dones.append(done)
+            obs, reward, done, _ = env.step(action)
+
+            rewards.append(reward)
+            actions.append(action)
+            # env.render()
+
+        # laatste turn voor game over
+        all_done.append(dones)
+        all_rewards.append(rewards)
+        all_obs.append(observations)
+        all_actions.append(actions)
+
+    print(all_done)
+    import time
+    time.sleep(1000)
+
+    all_obs = np.concatenate([np.concatenate([i]) for i in all_obs])
+    input = np.array(all_obs).reshape((-1, 4))
+
+    all_actions = np.concatenate([np.concatenate([i]) for i in all_actions])
+    output = np.array(all_actions).reshape((-1, 1))
+    print(output.shape)
 
 
-all_rewards = decay_and_normalize(all_rewards, 0.9)
+    all_rewards = decay_and_normalize(all_rewards, 0.97)
+    semi = model(input)
+    model.fit(input, all_actions)
+
 print(all_rewards)
--- models.py
+++ models.py
@@ -4,22 +4,22 @@ from funcs import super_loss
 
 def get_model(learningrate=3e-5, path=None):
     """Define and compile the tf model"""
-    neurons = 128
-    input = tf.keras.layers.Input(1)
+    neurons = 16
+    input = tf.keras.layers.Input(4)
 
     X = tf.keras.layers.Dense(neurons, "relu")(input)
     X = tf.keras.layers.Dense(neurons, "relu")(X)
     X = tf.keras.layers.Dense(neurons, "relu")(X)
 
     output = tf.keras.layers.Dense(neurons, "sigmoid")(X)
-    output = tf.keras.layers.Dense(1, "softmax", name="output")(output)
+    output = tf.keras.layers.Dense(1, "sigmoid", name="output")(output)
 
     agent = tf.keras.models.Model(inputs=[input], outputs=[output])
 
     if path:
         agent.load_weights(path)
 
-    agent.compile(tf.keras.optimizers.Adam(learningrate), super_loss)
+    agent.compile(tf.keras.optimizers.Adam(learningrate), "mse")
 
     return agent
 
